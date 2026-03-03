0521296f59a0f118b25c5e0bfe24dc0ad3caa6cc
JoaoVictor_Moraes <joaovictor.mroaes2728@gmail.com>
2026-03-02 21:19:04 -0300

    feat/logica/ajustando carrossel de ad's

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, DOCUMENT, ElementRef, Inject, QueryList, Renderer2, signal, ViewChild, ViewChildren } from '@angular/core';
+import { Component, DOCUMENT, ElementRef, Inject, Renderer2, signal, ViewChild, AfterViewInit, OnInit, OnDestroy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +7,7 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected readonly title = signal('first-angular-project');
 
@@ -16,12 +17,18 @@ export class App {
 
   ) { }
 
+  @ViewChild('containerCarousel') public carousel!: ElementRef;
+  private resizeObserver!: ResizeObserver;
+  private widthScreen!: number;
+  private countAnimation: number = 0;
+  private numberAnimationFixed: number = 1550
+  private numberAnimation!: number;
+  public animation: string = `translateX(1550px)`;
   public modalClose = {
     closeOne: false,
     closeTwo: false,
     shoList: false
   }
-
   public listAd = [
     false,
     true,
@@ -31,9 +38,30 @@ export class App {
     true,
   ]
 
-  private countAnimation: number = 0;
-  private numberAnimation: number = 0;
-  public animation: string = `translateX(0px)`;
+  ngOnInit(): void {
+    this.resizeObserver = new ResizeObserver(entries => {
+      for (const entry of entries) {
+        this.widthScreen = entry.contentRect.width;
+      }
+
+      if (this.widthScreen < 768) {
+        console.log('Mobile')
+        this.animation = 'translateX(800px)'
+
+      } else if (this.widthScreen >= 768 && this.widthScreen < 1280) {
+        console.log('Tablet')
+        this.animation = 'translate(1550px)'
+
+      } else {
+        alert('é Desktop Animal!!')
+
+      }
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.resizeObserver.observe(this.carousel.nativeElement)
+  }
 
   changeListNav() {
     if (this.modalClose.closeOne == false) {
@@ -43,33 +71,55 @@ export class App {
     } else {
       this.modalClose = { closeOne: false, closeTwo: false, shoList: false }
       this.renderer.removeClass(this.document.body, 'remove-scrool')
+
     }
 
   }
 
   changeCarouselRight() {
-    if (this.countAnimation < 5) {
-      this.numberAnimation -= 320;
-      this.animation = `translateX(${this.numberAnimation}px)`;
-      this.countAnimation++
+    // if (this.countAnimation < 5) {
+    //   this.calculateTranlateCarousel()
+    //   this.numberAnimationFixed -= this.numberAnimation;
+    //   this.animation = `translateX(${this.numberAnimationFixed}px)`;
+    //   this.countAnimation++
 
-      this.listAd[this.countAnimation - 1] = true;
-      this.listAd[this.countAnimation] = false;  
-      
-    }
+    //   this.listAd[this.countAnimation - 1] = true;
+    //   this.listAd[this.countAnimation] = false;
+    // }
   }
 
   changeCarouselLeft() {
-    if (this.countAnimation != 0) {
-      this.numberAnimation += 320
-      this.animation = `translateX(${this.numberAnimation}px)`;
-      this.countAnimation--
+    // if (this.countAnimation != 0) {
+    //   this.calculateTranlateCarousel()
+    //   this.numberAnimationFixed += this.numberAnimation
 
-      this.listAd[this.countAnimation + 1] = true;
-      this.listAd[this.countAnimation] = false;  
+    //   this.animation = `translateX(${this.numberAnimationFixed}px)`;
+    //   this.countAnimation--
+
+    //   this.listAd[this.countAnimation + 1] = true;
+    //   this.listAd[this.countAnimation] = false;
+
+    // }
+
+  }
+
+  calculateTranlateCarousel() {
+    if (this.widthScreen < 768) {
+      console.log('Mobile')
+      this.numberAnimation = 320
+
+    } else if (this.widthScreen >= 768 && this.widthScreen < 1280) {
+      console.log('Tablet')
+      this.numberAnimation = 620
+
+    } else {
+      alert('é Desktop Animal!!')
 
     }
+  }
 
+  ngOnDestroy(): void {
+    this.resizeObserver.disconnect();
   }
 
 }
